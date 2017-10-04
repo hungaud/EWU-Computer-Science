d9329171069004e1b20ac616c99cfac2ab911ee9
hungaud <hung.auduong@hotmail.com>
2017-10-04 16:47:48 -0700

    add lab 4

---
+++ CSCD 340 - Operating System/Lab4 - Process/tokenize/makeArgs.c
@@ -0,0 +1,53 @@
+#include "makeArgs.h"
+
+
+void clean(int argc, char **argv) {
+    int i;
+    for(i = 0; i < argc; i++) {
+        free(argv[i]);
+        argv[i] = NULL;
+    }
+    free(argv);
+}// end clean
+
+void printargs(int argc, char **argv) {
+	int x;
+	for(x = 0; x < argc; x++)
+		printf("%s\n", argv[x]);
+
+}// end printargs
+
+int makeargs(char *s, char *** argv) {
+    char copy [100];
+    int count = 0, i;
+    char * save;
+    strcpy(copy, s);
+    char * token = strtok_r(s," ", &save);
+    strip(token);
+    while(token != NULL) {
+        count++;
+        token = strtok_r(NULL, " ", &save);
+    }
+    if(count == 0) { 
+        return -1;
+    }
+    *argv = (char **)calloc(count+ 1, sizeof(char *));
+    token = strtok_r(copy, " ", &save);
+    strip(token);
+    (*argv)[0] = (char *)calloc(strlen(token) + 1, sizeof(char));
+    strcpy((*argv)[0], token);
+    for(i = 1; i < count; i++) {
+        token = strtok_r(NULL, " ", &save);
+        strip(token);
+        (*argv)[i] = (char *)calloc(strlen(token) + 1, sizeof(char));
+        strcpy((*argv)[i], token);
+    }
+    return count;
+}// end makeArgs
+
+
+
+
+
+
+
