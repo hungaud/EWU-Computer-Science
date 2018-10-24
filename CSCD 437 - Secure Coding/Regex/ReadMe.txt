7a604e35aa0645df223c3d4580b3318dd80579b4
hungaud <hung.auduong@hotmail.com>
2018-10-24 00:00:09 -0700

    finish regex

--- CSCD 437 - Secure Coding/Regex/ReadMe.txt
+++ CSCD 437 - Secure Coding/Regex/ReadMe.txt
@@ -1,2 +1,5 @@
 Things I was not able to do:
-6/7.) I wasnt sure if you needed a comma delimiter between the address so I did not include it.
+6/7.)       I wasnt sure if you needed a comma delimiter between the address so I did not include it.
+10.)        I couldn't figure out what was considered punctuation marks in the english dictionary
+            so instead I made sure it had \W Predefined character class. \W was the non-word character
+            so I used that as punctuations.
--- CSCD 437 - Secure Coding/Regex/src/Regex.java
+++ CSCD 437 - Secure Coding/Regex/src/Regex.java
@@ -8,18 +8,19 @@ public class Regex {
          "Date in MM-DD-YYYY", "House address", "City, State, Zip", "Military Time",
          "US Currency", "URL", "Password", "Odd Numbers of Characters Ending in \"ion\""
    };
+   private static HashSet<String> stateAbbrev;
 
    public static void main(String args[]) throws FileNotFoundException {
-      File file = new File("test.txt");
-      Scanner scan = new Scanner(file);
-      PrintStream writer = new PrintStream("result.txt");
       HashMap<String, ArrayList<String>> input = new HashMap<>();
-      initializeInputText(scan, input);
-      testFromFile(input, writer);
+      stateAbbrev = new HashSet<>();
+      initializeInputText(input);
+      testFromFile(input);
+      promptUserInputs();
    }
 
-   public static void initializeInputText(Scanner sc, HashMap<String, ArrayList<String>> input) {
-      String key = "a";
+   public static void initializeInputText(HashMap<String, ArrayList<String>> input) throws FileNotFoundException {
+      File file = new File("test.txt");
+      Scanner sc = new Scanner(file);String key = "a";
       while(sc.hasNextLine()) {
          String line = sc.nextLine();
          if(line.length() == 1 && line.matches("[a-l]")) {
@@ -30,16 +31,58 @@ public class Regex {
             input.get(key).add(line);
          }
       }
+      file = new File("StateAbbreviations.txt");
+      sc = new Scanner(file);
+      while(sc.hasNextLine()) {
+         stateAbbrev.add(sc.nextLine());
+      }
    }
 
-   public static void testFromFile(HashMap<String, ArrayList<String>> input, PrintStream writer) {
-      System.out.println("Testing with: \"result.txt\"");
+   public static void promptUserInputs() {
+      HashMap<String, ArrayList<String>> input;
+      String option = "", response = "";
+      Scanner sc = new Scanner(System.in);
+      System.out.println("");
+      String prompt ="User Input Test: \na\tSocial Security Number\nb\tUS Phone number\nc\tE-mail address\n" +
+            "d\tName on a class roster, assuming one or more middle initials - Last name, First name, MI\n" +
+            "e\tDate in MM-DD-YYYY format\n" +
+            "f\tHouse address - Street number, street name, abbreviation for road, street, boulevard or avenue" +
+            "g\tCity followed by state followed by zip as it should appear on a letter\n" +
+            "h\tMilitary time, including seconds\ni\tUS Currency down to the penny (ex: $123,456,789.23)\n" +
+            "j\tURL, including http:// (upper and lower case should be accepted)\n" +
+            "k\tA password that contains at least 10 characters and includes at least one upper case character, " +
+            "lower case character, digit, punctuation mark, and does not have more than " +
+            "3 consecutive lower case characters\nl\tAll words containing an odd number of " +
+            "alphabetic characters, ending in \"ion\"\nq\tQUIT";
+      while(!option.equals("q")) {
+         input = new HashMap<>();
+         System.out.println(prompt + "\n");
+         option = sc.next();
+         if(option.equals("q")) break;
+         response = sc.next();
+         input.put(option, new ArrayList<>());
+         input.get(option).add(response);
+         mapToTestCase(input, null);
+      }
+   }
+
+   public static void testFromFile(HashMap<String, ArrayList<String>> input) throws FileNotFoundException {
+      PrintStream writer = new PrintStream("result.txt");
+      System.out.println("Testing with: \"test.txt\"");
       writer.println("Regular Expressions");
+      mapToTestCase(input, writer);
+      System.out.println("Finished Testing with: \"test.txt\".\n" +
+            "The result should be in \"result.txt\"");
+   }
+
+   private static void mapToTestCase(HashMap<String, ArrayList<String>> input, PrintStream writer) {
       ArrayList<String> valid = new ArrayList<>();
       ArrayList<String> failed = new ArrayList<>();
       int index = 0;
       for(String key : input.keySet()) {
-         writer.println("\n" + (index + 1) + ". " + TITLE[index]);
+         if(writer != null) {
+            writer.println("\n" + (index + 1) + ". " + TITLE[index]);
+         }
          for(String value : input.get(key)) {
             switch(key) {
                case("a"):
@@ -81,14 +124,18 @@ public class Regex {
             }
          }
          index++;
-         writeResultToFile(valid, failed, writer);
+         if (writer != null)
+            writeResultToFile(valid, failed, writer);
+         else
+            printResultToConsole(valid, failed);
+
          valid.clear();
          failed.clear();
       }
    }
 
    public static void writeResultToFile(ArrayList<String> valid, ArrayList<String> failed,
-                                  PrintStream writer) {
+                                        PrintStream writer) {
       writer.println("Passed Tests:");
       for(String s : valid) {
          writer.println("\t" + s);
@@ -97,6 +144,9 @@ public class Regex {
       for(String s : failed) {
          writer.println("\t" + s);
       }
+   }
+   public static void printResultToConsole(ArrayList<String> valid, ArrayList<String> failed) {
+      System.out.println(failed.isEmpty() ? "Valid Response" : "Failed Response");
    }
 
    public static boolean isValidSocialSecurityNumber(String s) {
@@ -132,29 +182,35 @@ public class Regex {
       }
       return false;
    }
-//
+
    public static boolean isValidHouseAddress(String s) {
       return s.matches("^((\\d)+\\s[a-zA-Z0-9 ]+(rd|st|blvd|ave|road|street|boulevard|avenue)+.?)$");
    }
 
    public static boolean isValidCityStateZip(String s) {
-      return s.matches("^()$");
+      boolean valid = s.matches("^(([a-zA-Z,]+\\s){2}\\d{5}(-\\d{4})?)$");
+      String[] cityStateZip = s.split("\\s");
+      return valid && stateAbbrev.contains(cityStateZip[1].toUpperCase());
    }
 
    public static boolean isValidMilitaryTime(String s) {
-      return true;
+      boolean valid = s.matches("^([0-2]\\d(:[0-5]\\d){2})$");
+      String[] time = s.split(":");
+      return valid && Integer.parseInt(time[0]) < 24 ||
+            (time[0].equals("24") && time[1].equals("00") && time[2].equals("00"));
    }
 
    public static boolean isValidUSCurrency(String s) {
-      return true;
+      return s.matches("^(\\$\\d{1,3}(,\\d{3})*(.\\d{2}))$");
    }
 
    public static boolean isValidURL(String s) {
-      return true;
+      s = s.toLowerCase();
+      return s.matches("^((http://|https://)(www.)?([a-z0-9\\-]+)\\.([a-z0-9/.?=\\-])+)$");
    }
 
    public static boolean isValidPassword(String s) {
-      return true;
+      return s.matches("^((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.*[a-z]{3,})[\\w\\W]{10,})$");
    }
 
    public static boolean isValidOddLengthEndingIon(String s) {
